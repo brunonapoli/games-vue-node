85a837a46236d0a9c79d029bd0f98dd3fb532938
Bruno Napoli <napolii.bruno@gmail.com>
2022-03-23 10:21:12 -0300

    Configuro para que escuche en puerto 3030

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import app from "./server.js";
+
+function main() {
+    app.listen(app.get('port'));
+    console.log(`Server on port`, app.get('port'));
+};
+
+main();
---
+++ src/server.js
@@ -0,0 +1,12 @@
+import express, { urlencoded, json } from 'express';
+
+const app = express();
+
+// Configuración
+app.set('port', process.env.PORT || 3030);
+
+// Middleware
+app.use(urlencoded({ extended: false}));
+app.use(json());
+
+export default app;
