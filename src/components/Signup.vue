4f615d162bf51bde7d0a8d74da7905bc41be3c1e
Bruno Napoli <napolii.bruno@gmail.com>
2022-05-01 01:32:55 -0300

    Agrego validaciones en el usuario y mail al crear un nuevo usuario

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -6,20 +6,23 @@
 
       <b-form-group label="Ingrese su nombre de usuario" label-for="input-usuario"> 
         <b-form-input id="input-mail" v-model="usuario" type="text" placeholder="Ingrese su nombre de usuario"></b-form-input> 
+        <p class="error" v-if="errorUsuario" style="text-align: center; margin-top: 1%">{{errorUsuario}}</p>
       </b-form-group>
 
       <b-form-group label="Ingrese su mail" label-for="input-mail"> 
         <b-form-input id="input-mail" v-model="mail" type="text" placeholder="Ingrese su email"></b-form-input>
-        <p v-if="error" style="text-align: center; margin-top: 1%">Este usuario ya existe</p>
+        <p class="error" v-if="error" style="text-align: center; margin-top: 1%">Este usuario ya existe</p>
+        <p class="error" v-if="errorMail" style="text-align: center; margin-top: 1%">{{errorMail}}</p>
       </b-form-group>
 
       <b-form-group label="Ingrese su contraseña" label-for="input-contraseña"> 
         <b-form-input id="input-mail" v-model="contraseña" type="password" placeholder="Ingrese su contraseña"></b-form-input> 
+        <p class="error" v-if="errorContraseña"> {{errorContraseña}}</p>
       </b-form-group>
 
       <b-form-group label="Ingrese nuevamente su contraseña" label-for="input-repetirContraseña"> 
         <b-form-input id="input-mail" v-model="repetirContraseña" type="password" placeholder="Ingrese nuevamente su contraseña"></b-form-input> 
-        <p v-if="contraseña != repetirContraseña" style="text-align: center; margin-top: 10px"> {{errorContraseña}}</p>
+        <p class="error" v-if="contraseña != repetirContraseña"> {{errorContraseña}}</p>
       </b-form-group>
 
       <section class="row">
@@ -35,10 +38,6 @@
         </div>
       </section>
     </b-card>
-
-    <!-- <b-card v-if="error" bg-variant="danger" class="mx-auto" style="width: 500px; margin-top:5%; text-align: center;">
-      <h4>{{ error }}</h4>
-    </b-card> -->
     <Footer />
   </div>
 </template>
@@ -57,15 +56,56 @@ export default {
   data() {
     return {
       usuario: '',
+      errorUsuario: '',
       mail: '',
+      errorMail: '',
+      contraseña: '',
+      errorContraseña: '',
       repetirContraseña: '',
-      error:'',
-      errorContraseña: ''
+      error:''
     }
   },
   methods: {
+    errorPass() {
+      if (this.contraseña.length < 5) {
+        this.contraseña = ''
+        this.errorContraseña = 'Su contraseña debe tener al menos 5 letras/dígitos'
+        setTimeout(() => {
+          this.errorContraseña = '';
+        }, 1500);
+        return false
+      }
+      if (this.contraseña != this.repetirContraseña) {
+        this.contraseña = ''
+        this.errorContraseña = "Las contraseñas no coinciden"
+        setTimeout(() => {
+          this.errorContraseña = '';
+        }, 1500);
+        return false
+      }
+      return true
+    },
+    errorMailUsuario() {
+      if (this.usuario.length <= 3) {
+        this.errorUsuario = "El nombre del usuario debe contener al menos 4 letras/dígitos"
+        setTimeout(() => {
+          this.errorUsuario = '';
+        }, 1500);
+        return false
+      }
+      if (this.mail.includes('@') === false || this.mail.includes('.com') === false) {
+        this.errorMail = "Mail no reconocido"
+        setTimeout(() => {
+          this.errorMail = '';
+        }, 1500);
+        return false
+      }
+      return true
+    },
     registrar() {
-      if (this.contraseña === this.repetirContraseña) {
+      let errorDeContraseña = this.errorPass();
+      let errorDeUsuario = this.errorMailUsuario();
+      if (errorDeContraseña != false && errorDeUsuario != false) {
         let nuevoUsuario = {
           usuario: this.usuario, 
           mail: this.mail, 
@@ -84,13 +124,13 @@ export default {
             }, 1500);
           }
         )
-      } 
-      if (this.contraseña != this.repetirContraseña) {
-        this.errorContraseña = "Las contraseñas no coinciden"
-        setTimeout(() => {
-          this.errorContraseña = '';
-        }, 1500);
       }
+      // if (this.contraseña != this.repetirContraseña) {
+      //   this.errorContraseña = "Las contraseñas no coinciden"
+      //   setTimeout(() => {
+      //     this.errorContraseña = '';
+      //   }, 1500);
+      // }
     },
     ingresar() {
       this.$router.push('/');
@@ -114,5 +154,10 @@ li {
 }
 a {
   color: #42b983;
+}
+.error{
+  text-align: center; 
+  margin-top: 10px; 
+  color: rgb(255, 0, 0);
 }
 </style>
